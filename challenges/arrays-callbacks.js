bb62f4275c5f2cd0e74c3b298ebcf460fc4b79f5
Antoine Bibb <Czar@Jaeczar-imac.local>
2020-09-18 13:08:35 -0400

    bug free complete work

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -22,8 +22,8 @@ The zoos want to display both the scientific name and the animal name in front o
 */
 
 const displayNames = [];
-zooAnimals.forEach(function(animalName, scientificName){
-  return displayNames
+zooAnimals.forEach(function(item){
+  return displayNames.push(item.animal_name,item.scientific_name);
 })
 console.log(displayNames);
 
@@ -33,9 +33,13 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
   
-const lowCaseAnimalNames=
-zooAnimals.map(animalName=> name.zooAnimals.toLowerCase()); 
-console.log(lowCaseAnimalNames);
+const lowerCase = zooAnimals.map(
+  function(item){
+    return item.animal_name.toLowerCase();
+  }
+);
+
+console.log(lowerCase);
 
 /* Request 3: .filter() 
 
@@ -54,11 +58,11 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const reduceTotalPop = zooAnimals.reduce((total, zooAnimals)=>{
-  return total += zooAnimals.population;
+const totalPop = zooAnimals.reduce(function( accumulator , item){
+  return accumulator + item.population;
 },0);
-let populationTotal = 0;
-console.log(populationTotal);
+let population = 0;
+console.log(totalPop);
 
 
 // ==== Callbacks ====  
@@ -69,6 +73,9 @@ console.log(populationTotal);
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
+function consume (a, b, cb){
+  return cb(a,b);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -76,12 +83,19 @@ console.log(populationTotal);
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
-
+function add(a,b){
+  return a+b
+}
+function multiply(a,b){
+  return a*b
+}
+function greeting(a,b){
+  return `Hello ${a} ${b}, nice to meet you!`
+}
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
--- challenges/classes.js
+++ challenges/classes.js
@@ -1,29 +1,29 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
-class CuboidMaker{
+class CuboidMakerTwo{
     constructor(attr){
         this.length=attr.length;
         this.width=attr.width;
-        this.height=atr.height; 
+        this.height=attr.height; 
     }
     findVolume(){
-    return `${this.length}*${this.width}*${this.height}`;
+    return `${this.length*this.width*this.height}`;
   }
   findSurface(){
-    return `2*(${this.length}*${this.width}+${this.length}*${this.height}+${this.width}*${this.height})`;
+    return `${2*this.length*this.width+this.length*this.height+this.width*this.height}`;
   }
 }
-class Cuboid extends CuboidMaker{
+class Cuboid extends CuboidMakerTwo{
     constructor(attr){
         super(attr);
     }
 }
-const cuboid=new CuboidMaker({
+const cuboids = new CuboidMakerTwo({
   lenght:4,
   width:5,
   height:5,
 });
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+ console.log(cuboid.volume()); // 100
+ console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
